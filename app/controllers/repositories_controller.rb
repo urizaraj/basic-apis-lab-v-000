682774a4f4e2024399544cc81456aa3bdb56ffeb
Alexander Urizar <urizaraj@gmail.com>
2018-04-02 18:12:54 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,12 +1,9 @@
 class RepositoriesController < ApplicationController
-
-  def search
-
-  end
+  def search; end
 
   def github_search
     begin
-  @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
       req.params['client_id'] = client_id
       req.params['client_secret'] = client_secret
       req.params['v'] = '20160201'
@@ -14,16 +11,19 @@ class RepositoriesController < ApplicationController
       req.params['query'] = 'coffee shop'
       req.options.timeout = 0
     end
+
     body = JSON.parse(@resp.body)
+
     if @resp.success?
-      @venues = body["response"]["venues"]
+      @venues = body['response']['venues']
     else
-      @error = body["meta"]["errorDetail"]
+      @error = body['meta']['errorDetail']
     end
 
-  rescue Faraday::ConnectionFailed
-    @error = "There was a timeout. Please try again."
-  end
-  render 'search'
+    rescue Faraday::ConnectionFailed
+      @error = 'There was a timeout. Please try again.'
+    end
+
+    render 'search'
   end
 end
